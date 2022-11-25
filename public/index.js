c591f7d9b23c5a2347cd7e258f8eb32e9f58286c
yaddeepak <91468349+yaddeepak@users.noreply.github.com>
2022-11-25 15:48:03 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -16,7 +16,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://file-shar-app.herokuapp.com";
+const baseURL = "https://file-sharing-app-v1.vercel.app";
 // const baseURL = "http://localhost:3000";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
